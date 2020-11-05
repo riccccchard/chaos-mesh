4a7a6db59431d99b004061eabd2a60313aeafc3c
xiaoshu.peng@shopee.com <xiaoshu.peng@shopee.com>
2020-11-05 12:17:31 +0800

    update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,2 @@
-kubectl delete deploymen chaos-controller-manager chaos-dashboard -n chaos-testing
+kubectl delete deployment chaos-controller-manager chaos-dashboard -n chaos-testing
 kubectl delete daemonset chaos-daemon -n chaos-testing
